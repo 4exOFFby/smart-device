1dad57a3af2cfd5e5bd0cf05751edf1bf6eb87bc
Вася Чехов <4exoff@novoeradio.by>
2022-10-24 19:43:07 +0300

    Доделал весь функционал и сделал ПП

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,19 +1,25 @@
+
 const modal = document.querySelector('.modal-feedback');
 const buttonOpen = document.querySelector('.main-navigation__callback-button');
 const buttonClose = modal.querySelector('.feedback-form__button-close');
 const body = document.querySelector('body');
+const inputName = modal.querySelector('input[type="text"]');
 
 
 const modalOpenHandler = (evt) => {
   evt.preventDefault();
   body.classList.add('feedback-form--open');
   modal.classList.remove('modal-feedback--close');
+  inputName.focus();
   buttonClose.addEventListener('click', modalCloseHandler);
 };
 
 const modalCloseHandler = () => {
+  const inputNumber = modal.querySelector('input[name="user-number"]');
   modal.classList.add('modal-feedback--close');
   body.classList.remove('feedback-form--open');
+  inputNumber.value = '';
+  inputName.value = '';
 };
 
 buttonOpen.addEventListener('click', modalOpenHandler);
---
+++ source/js/phone-mask.js
@@ -0,0 +1,41 @@
+const renderPhoneNumberMask = () => {
+  [].forEach.call(document.querySelectorAll('input[name="user-number"]'), function (input) {
+    let keyCode;
+    function mask(evt) {
+      let pos = evt.target.selectionStart;
+      if (pos < 3) {
+        evt.preventDefault();
+      }
+      let matrix = '+7 (___) __-__-___';
+      let i = 0;
+      let def = matrix.replace(/\D/g, '');
+      let val = evt.target.value.replace(/\D/g, '');
+      let newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+      i = newValue.indexOf('_');
+      if (i !== -1) {
+        newValue = newValue.slice(0, i);
+      }
+      let reg = matrix.substr(0, evt.target.value.length).replace(/_+/g,
+          function (a) {
+            return '\\d{1,' + a.length + '}';
+          }).replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (!reg.test(evt.target.value) || evt.target.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        evt.target.value = newValue;
+      }
+      if (evt.type === 'blur' && evt.target.value.length < 5) {
+        evt.target.value = '';
+      }
+    }
+
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
+    input.addEventListener('keydown', mask, false);
+
+  });
+};
+
+export {renderPhoneNumberMask};
